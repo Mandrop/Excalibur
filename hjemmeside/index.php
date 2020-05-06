7c5b2e30fa33c81dc4f962069374110ca9963a9e
Mandrup <frederik.holstad@yahoo.dk>
2020-05-06 09:13:42 +0200

    papir

--- hjemmeside/index.php
+++ hjemmeside/index.php
@@ -31,7 +31,7 @@
 
 <!-- Modal -->
 <div class="modal fade" id="infoModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-scrollable" role="document">
+  <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalScrollableTitle">Information</h5>
