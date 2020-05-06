34804cb34867a84cc361fc1e10299e57e1142076
GrothDK <61182782+GrothDK@users.noreply.github.com>
2020-05-06 22:33:45 +0200

    update

---
+++ hjemmeside/templates/frames.php
@@ -0,0 +1,355 @@
+<!doctype html>
+<html lang="en" id="html">
+  <head>
+  
+  
+  <script src="Scripts/jquery-3.2.1.min.js" charset="utf-8"> </script>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    
+    <style>
+
+
+/* WOOD FRAME STYLE */
+.wood-frame img{
+  width:100%;
+}
+
+.wood-frame-content{
+  background:white url("../images/wood-frame/wood-frame-middle.jpg") repeat-y center;
+  background-size: 100%;
+  padding: 0 4rem 1px 4rem;
+}
+/* WOOD FRAME STYLE END */
+
+
+
+/* PERKAMENT FRAME */
+.perkament-frame img{
+  width:100%;
+}
+
+.perkament-frame-content{
+  background:transparent url("../images/perkament/perkament-center.png") repeat-y center;
+  background-size: 100%;
+  padding: 0 10% 1px 15%;
+}
+/* PERKAMENT FRAME END */
+
+
+
+    </style>
+
+    <title>Excalibur</title>
+  </head>
+  <body>
+
+
+
+<!-- WOOD FRAME START -->
+<!-- !!!!       Husk INTERNAL CSS i HEAD !!!!-->
+ 
+  <div class="wood-frame">
+
+      <img src="../images/wood-frame/wood-frame-top.jpg" alt="Wood frame top">
+
+      <div class="wood-frame-content">
+          <article>
+      <!-- CONTENT HERE -->
+
+        <h1>Dette er en test</h1>
+        <p>Dette er en test</p>
+
+                <h1>Dette er en test</h1>
+        <p>Dette er en test</p>
+
+      <!-- CONTENT HERE -->
+          </article>
+      </div>
+
+      <img src="../images/wood-frame/wood-frame-bottom.jpg" alt="Wood frame bottom">
+  </div>
+
+<!-- WOOD FRAME END -->
+
+
+<!-- PERKAMENT FRAME START -->
+<!-- !!!!       Husk INTERNAL CSS i HEAD !!!!-->
+ 
+  <div class="perkament-frame">
+
+      <img src="../images/perkament/perkament-top.png" alt="perkament frame top">
+
+      <div class="perkament-frame-content">
+          <article>
+      <!-- CONTENT HERE -->
+
+        <h1>Dette er en test</h1>
+        <p>Dette er en test</p>
+
+                <h1>Dette er en test</h1>
+        <p>Dette er en test</p>
+
+      <!-- CONTENT HERE -->
+          </article>
+      </div>
+
+      <img src="../images/perkament/perkament-bottom.png" alt="Perkament frame bottom">
+  </div>
+
+<!-- PERKAMENT FRAME END -->
+
+
+
+<!-- WOOD FRAME START -->
+<!-- !!!!       Husk INTERNAL CSS i HEAD !!!!-->
+ 
+  <div class="wood-frame">
+
+      <img src="../images/wood-frame/wood-frame-top.jpg" alt="Wood frame top">
+
+      <div class="wood-frame-content">
+          <article>
+      <!-- CONTENT HERE -->
+
+            <!-- PERKAMENT FRAME START -->
+            <!-- !!!!       Husk INTERNAL CSS i HEAD !!!!-->
+            
+              <div class="perkament-frame">
+
+                  <img src="../images/perkament/perkament-top.png" alt="perkament frame top">
+
+                  <div class="perkament-frame-content">
+                      <article>
+                  <!-- CONTENT HERE -->
+
+                    <h1>Dette er en test</h1>
+                    <p>Dette er en test</p>
+
+                            <h1>Dette er en test</h1>
+                    <p>Dette er en test</p>
+
+                  <!-- CONTENT HERE -->
+                      </article>
+                  </div>
+
+                  <img src="../images/perkament/perkament-bottom.png" alt="Perkament frame bottom">
+              </div>
+
+            <!-- PERKAMENT FRAME END -->
+
+
+      <!-- CONTENT HERE -->
+          </article>
+      </div>
+
+      <img src="../images/wood-frame/wood-frame-bottom.jpg" alt="Wood frame bottom">
+  </div>
+
+<!-- WOOD FRAME END -->
+
+
+
+
+
+<!-- WOOD FRAME START -->
+<!-- !!!!       Husk INTERNAL CSS i HEAD !!!!-->
+ 
+  <div class="wood-frame">
+
+      <img src="../images/wood-frame/wood-frame-top.jpg" alt="Wood frame top">
+
+      <div class="wood-frame-content">
+          <article>
+      <!-- CONTENT HERE -->
+
+
+          <div class="row">
+
+
+              <div class="col-6">
+                  <!-- PERKAMENT FRAME START -->
+                  <!-- !!!!       Husk INTERNAL CSS i HEAD !!!!-->
+                  
+                    <div class="perkament-frame">
+
+                        <img src="../images/perkament/perkament-top.png" alt="perkament frame top">
+
+                        <div class="perkament-frame-content">
+                            <article>
+                        <!-- CONTENT HERE -->
+
+                          <h1>Dette er en test</h1>
+                          <p>Dette er en test</p>
+
+                                  <h1>Dette er en test</h1>
+                          <p>Dette er en test</p>
+
+                        <!-- CONTENT HERE -->
+                            </article>
+                        </div>
+
+                        <img src="../images/perkament/perkament-bottom.png" alt="Perkament frame bottom">
+                    </div>
+
+                  <!-- PERKAMENT FRAME END -->
+               </div>
+
+
+              <div class="col-6">
+                  <!-- PERKAMENT FRAME START -->
+                  <!-- !!!!       Husk INTERNAL CSS i HEAD !!!!-->
+                  
+                    <div class="perkament-frame">
+
+                        <img src="../images/perkament/perkament-top.png" alt="perkament frame top">
+
+                        <div class="perkament-frame-content">
+                            <article>
+                        <!-- CONTENT HERE -->
+
+                          <h1>Dette er en test</h1>
+                          <p>Dette er en test</p>
+
+                                  <h1>Dette er en test</h1>
+                          <p>Dette er en test</p>
+
+                        <!-- CONTENT HERE -->
+                            </article>
+                        </div>
+
+                        <img src="../images/perkament/perkament-bottom.png" alt="Perkament frame bottom">
+                    </div>
+
+                  <!-- PERKAMENT FRAME END -->
+               </div>
+
+
+          </div>
+      <!-- CONTENT HERE -->
+          </article>
+      </div>
+
+      <img src="../images/wood-frame/wood-frame-bottom.jpg" alt="Wood frame bottom">
+  </div>
+
+<!-- WOOD FRAME END -->
+
+
+
+
+
+
+
+
+
+<!-- WOOD FRAME START -->
+<!-- !!!!       Husk INTERNAL CSS i HEAD !!!!-->
+ 
+  <div class="wood-frame">
+
+      <img src="../images/wood-frame/wood-frame-top.jpg" alt="Wood frame top">
+
+      <div class="wood-frame-content">
+          <article>
+      <!-- CONTENT HERE -->
+
+
+          <div class="row d-flex">
+
+
+              <div class="w-25">
+                  <!-- PERKAMENT FRAME START -->
+                  <!-- !!!!       Husk INTERNAL CSS i HEAD !!!!-->
+                  
+                    <div class="perkament-frame">
+
+                        <img src="../images/perkament/perkament-top.png" alt="perkament frame top">
+
+                        <div class="perkament-frame-content">
+                            <article>
+                        <!-- CONTENT HERE -->
+
+                          <h1>Dette er en test</h1>
+                          <p>Dette er en test</p>
+
+                                  <h1>Dette er en test</h1>
+                          <p>Dette er en test</p>
+
+                        <!-- CONTENT HERE -->
+                            </article>
+                        </div>
+
+                        <img src="../images/perkament/perkament-bottom.png" alt="Perkament frame bottom">
+                    </div>
+
+                  <!-- PERKAMENT FRAME END -->
+               </div>
+
+
+              <div class="w-25">
+                  <!-- PERKAMENT FRAME START -->
+                  <!-- !!!!       Husk INTERNAL CSS i HEAD !!!!-->
+                  
+                    <div class="perkament-frame">
+
+                        <img src="../images/perkament/perkament-top.png" alt="perkament frame top">
+
+                        <div class="perkament-frame-content">
+                            <article>
+                        <!-- CONTENT HERE -->
+
+                          <h1>Dette er en test</h1>
+                          <p>Dette er en test</p>
+
+                                  <h1>Dette er en test</h1>
+                          <p>Dette er en test</p>
+
+                        <!-- CONTENT HERE -->
+                            </article>
+                        </div>
+
+                        <img src="../images/perkament/perkament-bottom.png" alt="Perkament frame bottom">
+                    </div>
+
+                  <!-- PERKAMENT FRAME END -->
+               </div>
+
+
+          </div>
+      <!-- CONTENT HERE -->
+          </article>
+      </div>
+
+      <img src="../images/wood-frame/wood-frame-bottom.jpg" alt="Wood frame bottom">
+  </div>
+
+<!-- WOOD FRAME END -->
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    
+    
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+    <script src="https://code.jquery.com/jquery-3.5.0.min.js"></script>
+
+    <script src="Scripts/waypoint/lib/jquery.waypoints.min.js"></script>
+    
+  </body>
+</html>
