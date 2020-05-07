3826db3cf6291c61ae6ca2fc27ca5b06aff39fca
GrothDK <61182782+GrothDK@users.noreply.github.com>
2020-05-07 13:40:59 +0200

    update

---
+++ hjemmeside/templates/nyheder.php
@@ -0,0 +1,249 @@
+<!doctype html>
+<html lang="en" id="html">
+  <head>
+  
+  
+  <script src="Scripts/jquery-3.2.1.min.js" charset="utf-8"> </script>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    
+    <style>
+
+
+/* WOOD FRAME STYLE */
+.wood-frame img{
+  width:100%;
+}
+
+.wood-frame-content{
+  background:white url("../images/wood-frame/wood-frame-middle.png") repeat-y center;
+  background-size: 100%;
+  padding: 1px 4rem 1px 4rem;
+}
+/* WOOD FRAME STYLE END */
+
+
+
+/* PERKAMENT FRAME */
+.perkament-frame img{
+  width:100%;
+}
+
+.perkament-frame-content{
+  background:transparent url("../images/perkament/perkament-center.png") repeat-y center;
+  background-size: 100%;
+  padding: 1px 10% 1px 10%;
+}
+/* PERKAMENT FRAME END */
+
+
+/* NYHEDER */
+
+.nyheder .card{
+/*     background-color: #ffe151; */
+ /*   background-color: #ffe151e6; */
+  background-color: #ffffffd4;
+}
+.nyheder .card:hover{
+    background-color: #ffe151e0; 
+}
+
+
+.bg-opacity-80:before{
+    opacity: .5;
+}
+
+.news-date{
+    top: -30px;
+}
+
+.object-fit-news-card{
+    object-fit: cover;
+    height: 225px;
+}
+
+.news-element2 img{
+    object-fit: cover;
+    height: 150px;
+}
+
+.news-element2 .news-date{
+    top: 120px;
+}
+
+.event-commercial{
+    background: white url(../images/group-of-people-doing-marathon.jpg) no-repeat center center /cover;
+    color: #ffe151;
+    min-height: 225px;
+}
+
+/* .event-commercial:hover{
+    color: white;
+} */
+
+
+/* NYHEDER END */
+
+
+
+    </style>
+
+    <title>Excalibur</title>
+  </head>
+  <body>
+
+
+
+
+
+
+
+<br>
+
+
+<!-- WOOD FRAME START -->
+<!-- !!!!       Husk INTERNAL CSS i HEAD !!!!-->
+ 
+  <div class="wood-frame">
+
+      <img src="../images/wood-frame/wood-frame-top.png" alt="Wood frame top">
+
+      <div class="wood-frame-content">
+          <article>
+      <!-- CONTENT HERE -->
+
+
+          <div class="row">
+
+
+              <div class="col-6">
+                  <!-- PERKAMENT FRAME START -->
+                  <!-- !!!!       Husk INTERNAL CSS i HEAD !!!!-->
+                  
+                    <div class="perkament-frame">
+
+                        <img src="../images/perkament/perkament-top.png" alt="perkament frame top">
+
+                        <div class="perkament-frame-content">
+                            <article>
+                        <!-- CONTENT HERE -->
+
+                            <!-- NEWS CARD -->
+                                        <div class="w-100 bg-opacity m-2">
+                                            <img class="rounded w-100 object-fit-news-card" src="../images/roleplay-news.jpg" alt="">
+
+                                            <div class="row m-0 position-relative">
+
+                                                <div class="position-absolute news-date d-flex align-items-center bg-warning">
+                                                    <p class="m-0 font-weight-bold">4. JANUAR 2020</p>
+                                                            
+                                                </div>
+
+                                                <div class="col p-0">
+                                                    <h4 class="mb-0 p-2">
+                                                        Nyt marathon kommer til viborg
+                                                    </h2>
+                                                </div>
+
+                                            </div>
+
+                                        </div>
+                            <!-- NEWS CARD END -->
+
+                        <!-- CONTENT HERE -->
+                            </article>
+                        </div>
+
+                        <img src="../images/perkament/perkament-bottom.png" alt="Perkament frame bottom">
+                    </div>
+
+                  <!-- PERKAMENT FRAME END -->
+               </div>
+
+
+              <div class="col-6">
+                  <!-- PERKAMENT FRAME START -->
+                  <!-- !!!!       Husk INTERNAL CSS i HEAD !!!!-->
+                  
+                    <div class="perkament-frame">
+
+                        <img src="../images/perkament/perkament-top.png" alt="perkament frame top">
+
+                        <div class="perkament-frame-content">
+                            <article>
+                        <!-- CONTENT HERE -->
+
+                            <!-- NEWS CARD -->
+                                        <div class="w-100 bg-opacity m-2">
+                                            <img class="rounded w-100 object-fit-news-card" src="../images/roleplay-news.jpg" alt="">
+
+                                            <div class="row m-0 position-relative">
+
+                                                <div class="position-absolute news-date d-flex align-items-center bg-warning">
+                                                    <p class="m-0 font-weight-bold">4. APRIL 2020</p>
+                                                            
+                                                </div>
+
+                                                <div class="col p-0">
+                                                    <h4 class="mb-0 p-2">
+                                                        Nyt marathon kommer til viborg
+                                                    </h2>
+                                                </div>
+
+                                            </div>
+
+                                        </div>
+                            <!-- NEWS CARD END -->
+
+
+
+
+                        <!-- CONTENT HERE -->
+                            </article>
+                        </div>
+
+                        <img src="../images/perkament/perkament-bottom.png" alt="Perkament frame bottom">
+                    </div>
+
+                  <!-- PERKAMENT FRAME END -->
+               </div>
+
+
+          </div>
+      <!-- CONTENT HERE -->
+          </article>
+      </div>
+
+      <img src="../images/wood-frame/wood-frame-bottom.png" alt="Wood frame bottom">
+  </div>
+
+<!-- WOOD FRAME END -->
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    
+    
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+    <script src="https://code.jquery.com/jquery-3.5.0.min.js"></script>
+
+    <script src="Scripts/waypoint/lib/jquery.waypoints.min.js"></script>
+    
+  </body>
+</html>
