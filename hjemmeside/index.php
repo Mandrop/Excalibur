3bd22a92449be7a2b6fac99bc79632b91ae6d4be
Mandrup <frederik.holstad@yahoo.dk>
2020-05-07 09:14:20 +0200

    123

--- hjemmeside/index.php
+++ hjemmeside/index.php
@@ -124,30 +124,122 @@ Du kan finde telefonnumre og mail-adresser på foreningens afviklere under Konta
 
 <div class="second" id="second"></div>
 <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
-  Launch demo modal
-</button>
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
+<footer id="kontakt">
+      <div class="container">
+  
+        <!-- INFORMATION CARD DECK -->
+        <div class="card-deck pt-4">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">TELEFON</h5>
+              <p class="card-text">Sigurd - 12 34 56 78</p>
+              <p class="card-text">Jonas - 15 34 56 68</p>
+              <p class="card-text">Michael - 32 44 66 38</p>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">EMAIL</h5>
+              <p class="card-text">indmelding@excalibur.dk</p>
+              <p class="card-text">praktisk@excalibur.dk</p>
+            </div>
+          </div>
+          
+          <!--<div class="card">
+            <div class="card-body">
+              <h5 class="card-title">ADRESSE</h5>
+              <p class="card-text">Kærbyvej 7</p>
+              <p class="card-text">2610 Rødovre</p>
+            </div>
+          </div>-->
+
+
+        </div>
+        
+        <h5 class="text-center">Spillesteder</h5>
+
+        <!-- SPILLESTEDER -->
+        <div class="row row-cols-1 row-cols-md-2">
+  <div class="col mb-4">
+    <div class="card">
+      <!--<img src="..." class="card-img-top" alt="...">-->
+      <iframe class="card-img-top" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d18134.45889417193!2d10.323192105196053!3d55.378818031556314!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464cdfbb0a9b29f3%3A0xc9693e9382f41e9f!2sTusind%C3%A5rsskoven!5e0!3m2!1sda!2sdk!4v1588762046567!5m2!1sda!2sdk" width="300" height="280" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+      <div class="card-body">
+        <h5 class="card-title">Tusindeårsskoven</h5>
+        <h6>Odense SV, 5250</h6>
+        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi exercitationem, architecto repudiandae et quos consequatur maiores dolorem repellat nisi odit necessitatibus quae dicta a voluptate, amet perspiciatis voluptatem nostrum magnam provident corrupti quam assumenda! Et deleniti aliquid omnis impedit ipsam.</p>
       </div>
-      <div class="modal-body">
-        <p>Test</p>
+    </div>
+  </div>
+  <div class="col mb-4">
+    <div class="card">
+      <!--<img src="..." class="card-img-top" alt="...">-->
+      <iframe class="card-img-top" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d18134.45889417193!2d10.323192105196053!3d55.378818031556314!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464cdfbb0a9b29f3%3A0xc9693e9382f41e9f!2sTusind%C3%A5rsskoven!5e0!3m2!1sda!2sdk!4v1588762046567!5m2!1sda!2sdk" width="300" height="280" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+      <div class="card-body">
+        <h5 class="card-title">Tusindeårsskoven</h5>
+        <h6>Odense SV, 5250</h6>
+        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi exercitationem, architecto repudiandae et quos consequatur maiores dolorem repellat nisi odit necessitatibus quae dicta a voluptate, amet perspiciatis voluptatem nostrum magnam provident corrupti quam assumenda! Et deleniti aliquid omnis impedit ipsam.</p>
       </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+    </div>
+  </div>
+  <div class="col mb-4">
+    <div class="card">
+      <!--<img src="..." class="card-img-top" alt="...">-->
+      <iframe class="card-img-top" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d18134.45889417193!2d10.323192105196053!3d55.378818031556314!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464cdfbb0a9b29f3%3A0xc9693e9382f41e9f!2sTusind%C3%A5rsskoven!5e0!3m2!1sda!2sdk!4v1588762046567!5m2!1sda!2sdk" width="300" height="280" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+      <div class="card-body">
+        <h5 class="card-title">Tusindeårsskoven</h5>
+        <h6>Odense SV, 5250</h6>
+        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi exercitationem, architecto repudiandae et quos consequatur maiores dolorem repellat nisi odit necessitatibus quae dicta a voluptate, amet perspiciatis voluptatem nostrum magnam provident corrupti quam assumenda! Et deleniti aliquid omnis impedit ipsam.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col mb-4">
+    <div class="card">
+      <!--<img src="..." class="card-img-top" alt="...">-->
+      <iframe class="card-img-top" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d18134.45889417193!2d10.323192105196053!3d55.378818031556314!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464cdfbb0a9b29f3%3A0xc9693e9382f41e9f!2sTusind%C3%A5rsskoven!5e0!3m2!1sda!2sdk!4v1588762046567!5m2!1sda!2sdk" width="300" height="280" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+      <div class="card-body">
+        <h5 class="card-title">Tusindeårsskoven</h5>
+        <h6>Odense SV, 5250</h6>
+        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi exercitationem, architecto repudiandae et quos consequatur maiores dolorem repellat nisi odit necessitatibus quae dicta a voluptate, amet perspiciatis voluptatem nostrum magnam provident corrupti quam assumenda! Et deleniti aliquid omnis impedit ipsam.</p>
       </div>
     </div>
   </div>
 </div>
+
+        <!-- KONTAKTFORMULAR -->
+        
+      <form class="text-center mt-5 pb-4 contact-form mb-5" action="index.php">
+        <h2 class="pb-4">Send Os En Besked</h2>
+
+        <div class="form-group">
+          <input type="text" class="form-control" id="inputAddress" placeholder="Navn" required>
+        </div>
+
+        <div class="form-group">
+          <input type="email" class="form-control" id="inputAddress" placeholder="Email" required>
+        </div>
+
+        <div class="form-group">
+          <input type="tel" class="form-control" id="inputAddress" placeholder="Telefonnumer" required>
+        </div>
+
+        <div class="form-group">
+          <textarea class="form-control" rows="5" cols="50"id="inputAddress" placeholder="Besked" required></textarea>
+
+        </div>
+
+        <div class="text-center">
+          <button type="submit" class="btn btn-primary">SEND BESKED</button>
+        </div>
+      </form>
+
+
+      </div>
+      <div class="copyright text-center">© 2020</div>
+    </footer>
+
 
 
     <!-- Optional JavaScript -->
