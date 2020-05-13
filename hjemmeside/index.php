a7d6aac1ede10794977ca54edc60f75aa30dfcb4
Mandrup <frederik.holstad@yahoo.dk>
2020-05-13 12:47:12 +0200

    l

--- hjemmeside/index.php
+++ hjemmeside/index.php
@@ -89,8 +89,9 @@
 
   <!-- FADE IN START -->
   <div class="background-fade-in">
-    <div class="gradient-one"></div>
-    <div class="gradient-two"></div>
+    <!--<div class="gradient-one"></div>
+    <div class="gradient-two"></div>-->
+    <img class="background-image absolute" src="Images/background/top-part-gradient.png" alt="">
     <img class="background-image" src="Images/background/top-part-v2.jpg" alt="">
 
   </div>
@@ -125,12 +126,22 @@
     
       <img class="barn-img" src="Images/background/barn-farm-v2.png" alt="">
     </div>
+
+    
     
   </div>
 
 
     <div class="background" id="background">
-      
+
+    <!-- BJERG OG ORKBY IMAGE -->
+    <img class="background-image" src="Images/background/del2/BG-del2-EDIT.jpg" alt="">
+
+    <!-- BORG IMAGE -->
+    <div class="relative">
+      <img class="background-image" src="Images/background/del3/BG-del3-BORG.png" alt="">
+      <img class="borgmur" src="Images/background/del3/borg.png" alt="">
+    </div>  
 
 
 
